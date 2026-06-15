ff231409d0241d50fb496cff8871fd9592aefc37
edokamezie@gmail.com <edokamezie@gmail.com>
2026-06-15 02:36:28 +0000

    Table and View materialization

---
+++ models/demo/customer.sql
@@ -0,0 +1,20 @@
+{{
+    config
+    (
+        materialized = 'table'
+    )
+}}
+with customer_src as 
+(
+select    
+    CUSTOMER_ID,
+    FIRST_NAME,
+    LAST_NAME,
+    EMAIL,
+    PHONE,
+    COUNTRY,
+    CREATED_AT,
+    CURRENT_TIMESTAMP as INSERT_DTS
+from {{source('customerx', 'CUSTOMER_SRC')}}
+)
+select * from customer_src
